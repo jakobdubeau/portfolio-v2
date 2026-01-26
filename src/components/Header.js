b3cf5df968f42f0d6c04e2a3bf9a58cb44251fb3
jakobdubeau <jakobdubeau@gmail.com>
2026-01-25 21:42:11 -0500

    initial header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ export default function Header() {
 
   const links = [
     {
-      name: "about",
+      name: "[about]",
       href: "/",
       isActive: pathname === "/"
     },
     {
-      name: "projects",
+      name: "[projects]",
       href: "/projects",
       isActive: pathname === "/projects"
     },
@@ -20,9 +20,22 @@ export default function Header() {
 
   return (
     <div className="flex justify-between items-center">
-      <h1 className="text-red-500 font-extrabold">
-        <Link href="/">Jakob Dubeau</Link>
+      <h1 className="text-stone-200 hover:text-white duration-200 transition-all font-semibold">
+        <Link href="/">jakob dubeau</Link>
       </h1>
+			<div className="flex items-center gap-2 text-sm md:gap-4">
+        {links.map((link) => (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={`text-stone-400 hover:text-white duration-200 transition-all ${
+              link.isActive ? "underline-offset-2" : ""
+            }`}
+          >
+            {link.name}
+          </Link>
+        ))}
+			</div>
     </div>
   )
 }
