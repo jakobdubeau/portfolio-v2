cb898c19214082ccfe7a39e07ef639848c70d795
jakobdubeau <jakobdubeau@gmail.com>
2026-01-27 20:57:35 -0500

    initial bullet animations

--- src/app/page.js
+++ src/app/page.js
@@ -6,33 +6,33 @@ export default function Home() {
   return (
     <div className="flex flex-col pt-6 mx-10">
       <ul className="grid gap-2 text-base">
-        <li className="flex items-baseline gap-3 pl-6">
-          <span className="text-white font-bold">
+        <li className="flex items-baseline gap-3 pl-6 group">
+          <span className="text-white font-bold group-hover:translate-x-2 transition-transform duration-200 ease-out">
             &gt;
           </span>
-          <span className="text-stone-400">
+          <span className="text-stone-400 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             CS @
           </span>
-          <span className="text-stone-300 font-medium">
+          <span className="text-stone-300 font-medium group-hover:translate-x-2 transition-transform duration-200 ease-out">
             University of Ottawa
           </span>
         </li>
-        <li className="flex items-baseline gap-3 pl-6">
-          <span className="text-white font-bold">
+        <li className="flex items-baseline gap-3 pl-6 group">
+          <span className="text-white font-bold group-hover:translate-x-2 transition-transform duration-200 ease-out">
             &gt;
           </span>
-          <span className="text-stone-400">
+          <span className="text-stone-400 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             seeking
           </span>
-          <span className="text-stone-300 font-medium">
+          <span className="text-stone-300 font-medium group-hover:translate-x-2 transition-transform duration-200 ease-out">
             S26/F26
           </span>
-          <span className="text-stone-400">
+          <span className="text-stone-400 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             internships
           </span>
         </li>
-        <li className="flex flex-col gap-2 pl-6">
-          <div className="flex items-baseline gap-3">
+        <li className="flex flex-col gap-2 pl-6 group">
+          <div className="flex items-baseline gap-3 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             <span className="text-white font-bold">
               &gt;
             </span>
@@ -43,7 +43,7 @@ export default function Home() {
               interests:
             </span>
           </div>
-          <ul className="grid gap-1 pl-6">
+          <ul className="grid gap-1 pl-6 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             <li className="relative flex items-start gap-4">
               <span className="text-stone-300 font-normal">
                 *
@@ -70,14 +70,14 @@ export default function Home() {
             </li>
           </ul>
         </li>
-        <li className="flex items-baseline gap-3 pl-6">
-          <span className="text-white font-bold">
+        <li className="flex items-baseline gap-3 pl-6 group">
+          <span className="text-white font-bold group-hover:translate-x-2 transition-transform duration-200 ease-out">
             &gt;
           </span>
-          <span className="text-stone-400">
+          <span className="text-stone-400 group-hover:translate-x-2 transition-transform duration-200 ease-out">
             building
           </span>
-          <span className="text-stone-400 font-medium underline underline-offset-2 hover:text-neutral-200 transition-all duration-200">
+          <span className="text-stone-400 font-medium underline underline-offset-2 group-hover:translate-x-2 ease-out hover:text-neutral-200 transition-all duration-200">
             <Link
               href="https://github.com/jakobdubeau/kora"
               target="_blank"
